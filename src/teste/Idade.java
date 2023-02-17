95dd15e574203ec95c161087fe5b35a93591cacf
IagoMartuci <martuciiago1992@gmail.com>
2023-02-17 01:41:46 -0300

    VerificaIdade

---
+++ src/teste/Idade.java
@@ -0,0 +1,11 @@
+package teste;
+
+public class Idade {
+    public static void main(String[] args){
+        var menorIdade = 15;
+        var maiorIdade = 20;
+
+        if (maiorIdade >= 18)
+            System.out.println("Maior idade: " + maiorIdade + " anos");
+    }
+}
